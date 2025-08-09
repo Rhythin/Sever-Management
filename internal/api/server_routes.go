94a0dd76867da7a455a080a68e1e448cbfa757ad
Rhythin <rhythin.renny@converj.com>
2025-08-09 19:50:53 +0530

    slight modification to server routes

--- internal/api/server_routes.go
+++ internal/api/server_routes.go
@@ -11,10 +11,13 @@ func NewServerRouter(h *ServerHandlers) http.Handler {
 	r := chi.NewRouter()
 
 	r.Post("/server", h.ProvisionServer)
-	r.Get("/servers/{id}", h.GetServer)
-	r.Post("/servers/{id}/action", h.ServerAction)
-	r.Get("/servers", h.ListServers)
-	r.Get("/servers/{id}/logs", h.GetServerLogs)
+
+	r.Route("/servers", func(r chi.Router) {
+		r.Get("/", h.ListServers)
+		r.Get("/{id}", h.GetServer)
+		r.Post("/{id}/action", h.ServerAction)
+		r.Get("/{id}/logs", h.GetServerLogs)
+	})
 
 	return r
 }
